8f23ae8aa82f234b5168c013adbed449e0b538a9
chandresh tekchandani <ctekchandani@italentdigital.com>
2022-06-09 18:14:51 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+using Progress.Sitefinity.AspNetCore;
+using Progress.Sitefinity.AspNetCore.FormWidgets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSitefinity();
+builder.Services.AddViewComponentModels();
+builder.Services.AddFormViewComponentModels();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSitefinity();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapSitefinityEndpoints();
+});
+
+app.Run();
