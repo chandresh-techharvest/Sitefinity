e0798a1ad589c748efaa89e47b48595fb621a7d3
chandresh tekchandani <ctekchandani@italentdigital.com>
2022-06-17 09:28:12 +0530

    Updates Till 17-06-2022

---
+++ Core Renderer/Renderer/Renderer/ViewComponents/HelloWorldViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progress.Sitefinity.AspNetCore.ViewComponents;
+using Renderer.Entities;
+
+namespace Renderer.ViewComponents
+{
+    [SitefinityWidget]
+    public class HelloWorldViewComponent : ViewComponent
+    {
+        // Invokes the view.
+        public IViewComponentResult Invoke(IViewComponentContext<HelloWorldEntity> context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return this.View(context.Entity);
+        }
+    }
+}
