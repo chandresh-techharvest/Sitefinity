946190cec43785f39f06f2400e0d88add9728b01
chandresh tekchandani <ctekchandani@italentdigital.com>
2022-06-30 09:48:46 +0530

    New Custom Control, Custom Module Our Speakers and new form

---
+++ Core Renderer/Renderer/Renderer/Entities/CustomNewsEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Renderer.Entities
+{
+    public class CustomNewsEntity
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string SpeakersLink { get; set; }
+        public string ScheduleLink { get; set; }
+        public DateTime NewsDate { get; set; }
+        public string NewsOwner { get; set; }
+
+    }
+}
---
+++ Core Renderer/Renderer/Renderer/ViewComponents/CustomNewsViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progress.Sitefinity.AspNetCore.ViewComponents;
+using Renderer.Entities;
+
+namespace Renderer.ViewComponents
+{
+
+    [SitefinityWidget]
+    public class CustomNewsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IViewComponentContext<CustomNewsEntity> context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.Entity.Title = "The importance of Leadership Conference in 2022";
+            context.Entity.Body = "Leadership Event is one-page Bootstrap v5.1.3 CSS layout for your website. Thank you for choosing TemplateMo website where you can instantly download free CSS templates at no cost.";
+            context.Entity.NewsDate = DateTime.Now;
+            context.Entity.ScheduleLink = "#section_4";
+            context.Entity.SpeakersLink = "#section_3";
+
+            return this.View(context.Entity);
+        }
+    }
+}
+
